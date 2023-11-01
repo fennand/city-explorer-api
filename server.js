a6065505b76597c287aa3ba2c41257b83187d682
Andrew Fennell <afennell@hotmail.co.uk>
2023-11-01 19:46:36 +0000

    Add live weather

--- server.js
+++ server.js
@@ -8,12 +8,21 @@ const data = require("./data/weather.json");
 
 app.get("/", (_, response) => response.json("Root route."));
 
-app.get("/weather", (request, response) => {
+app.get("/weather", async (request, response) => {
   const lat = request.query.lat;
   const lon = request.query.lon;
-  const searchQuery = request.query.searchQuery;
-
-  const filteredCity = data.find((city) => {
+  //const searchQuery = request.query.searchQuery;
+  const API = `https://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+  const res = await axios.get(API);
+  const weatherData = res.data.data.map((place) => {
+    return {
+      description: place.weather.description,
+      date: place.datetime,
+      temp: place.temp,
+    };
+  });
+  response.json(weatherData);
+  /* const filteredCity = data.find((city) => {
     return (
       city.city_name === searchQuery //&& city.lat === lat && city.lon === lon
     );
@@ -26,7 +35,7 @@ app.get("/weather", (request, response) => {
     };
   });
 
-  response.json(wrangledData);
+  response.json(wrangledData); */
 });
 
 app.listen(PORT, () => console.log(`App is running in PORT: ${PORT}`));
